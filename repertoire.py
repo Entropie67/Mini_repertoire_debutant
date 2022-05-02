4f5a64e22374671dcc1c6e03a8dd8d6ee40de88f
Entropie67 <olivier.elophe@gmail.com>
2022-05-02 13:42:16 +0200

    Suppression de doubles points inutiles

--- repertoire.py
+++ repertoire.py
@@ -2,8 +2,8 @@
 # Mini projet débutant.
 import json
 print('#'*50)
-print("\n Répertoir trop puissant |\n version réalisée par Red Richards, alias\
-    Mr Fantastique à l'occasion de son apparition dans Docteur Strange 2\n")
+print("\n Répertoir trop puissant |\n version réalisée par Red Richards, \n alias \
+ Mr Fantastique à l'occasion de son apparition dans Docteur Strange 2\n")
 print('#'*50)
 
 def ajouter_nom():
@@ -24,9 +24,9 @@ def rechercher_nom():
 choix = None
 while choix != 0:
     print(" \nVeuillez choisir une option\n")
-    print(" 1 - Ajouter un nom:")
-    print(" 2 - Consulter le répertoire:")
-    print(" 0 - Pour quitter :")
+    print(" 1 - Ajouter un nom")
+    print(" 2 - Consulter le répertoire")
+    print(" 0 - Pour quitter")
     choix = input("-->")
     if choix in [0, 1, 2]:
         print(f"Votre choix est le {choix}")
