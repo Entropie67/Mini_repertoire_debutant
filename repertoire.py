b8c77c145b6f08ce09026f65597bd75a9708fddd
Entropie67 <olivier.elophe@gmail.com>
2022-05-02 13:40:10 +0200

    quelques fonctions et presque signatures des dites fonctions

--- repertoire.py
+++ repertoire.py
@@ -1,11 +1,22 @@
 
 # Mini projet débutant.
-
+import json
 print('#'*50)
 print("\n Répertoir trop puissant |\n version réalisée par Red Richards, alias\
     Mr Fantastique à l'occasion de son apparition dans Docteur Strange 2\n")
 print('#'*50)
 
+def ajouter_nom():
+    pass
+
+def afficher_repertoir():
+    pass
+
+def detruire_urgence():
+    pass
+
+def rechercher_nom():
+    pass
 
 #######################
 #       Menu          #
