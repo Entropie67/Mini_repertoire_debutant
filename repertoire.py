cd52cc1a73491001cf16758986feb8b4ec3acb54
Entropie67 <olivier.elophe@gmail.com>
2022-05-02 13:32:20 +0200

    Base

---
+++ repertoire.py
@@ -0,0 +1,12 @@
+
+# Mini projet débutant.
+
+print('#'*50)
+print("\n Répertoir trop puissant |\n version réalisée par Red Richards, alias\
+    Mr Fantastique à l'occasion de son apparition dans Docteur Strange 2\n")
+print('#'*50)
+
+print(" Veuillez choisir une option :")
+print(" 1 - Ajouter un nom:")
+print(" 2 - Consulter le répertoire:")
+print(" 0 - Pour quitter :")
