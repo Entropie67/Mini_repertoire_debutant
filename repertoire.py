1c374ed8c1fb1e0a44f2f1ea028d3a3072b4f3d0
Entropie67 <olivier.elophe@gmail.com>
2022-05-02 14:00:30 +0200

    Ajout d'un nom

--- repertoire.py
+++ repertoire.py
@@ -6,10 +6,12 @@ print("\n Répertoir trop puissant |\n version réalisée par Red Richards, \n a
  Mr Fantastique à l'occasion de son apparition dans Docteur Strange 2\n")
 print('#'*50)
 
-def ajouter_nom():
-    nom  = input("Nom : ")
-    numero = input(f"Numéro de {nom}")
-
+def ajouter_nom(data):
+    nom = input("Nom : ")
+    numero = input(f"Numéro de {nom} :")
+    data[nom] = numero
+    with open('data.json', 'w') as fp:
+        json.dump(data, fp)
 
 def afficher_repertoir():
     pass
@@ -20,20 +22,25 @@ def detruire_urgence():
 def rechercher_nom():
     pass
 
+with open('data.json', 'r') as fp:
+    data = json.load(fp)
+choix = None
+print(data)
 #######################
 #       Menu          #
 #######################
-choix = None
+
+
 while choix != 0:
     print(" \nVeuillez choisir une option\n")
     print(" 1 - Ajouter un nom")
     print(" 2 - Consulter le répertoire")
     print(" 0 - Pour quitter")
-    choix = input("-->")
+    choix = int(input("-->"))
     if choix in [0, 1, 2]:
         print(f"Votre choix est le {choix}")
         if choix == 1:
             print("Vous débutez la procédure d'ajout d'un nouveau nom")
-            ajouter_nom()
+            ajouter_nom(data)
     else:
         print("Choix invalide, veuillez recommencer")
