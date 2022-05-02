896d5f7ff846024fce1b95ed0f6e88d47ba758f2
Entropie67 <olivier.elophe@gmail.com>
2022-05-02 13:35:04 +0200

    Première boucle

--- repertoire.py
+++ repertoire.py
@@ -5,8 +5,12 @@ print('#'*50)
 print("\n Répertoir trop puissant |\n version réalisée par Red Richards, alias\
     Mr Fantastique à l'occasion de son apparition dans Docteur Strange 2\n")
 print('#'*50)
+choix = None
 
-print(" Veuillez choisir une option :")
-print(" 1 - Ajouter un nom:")
-print(" 2 - Consulter le répertoire:")
-print(" 0 - Pour quitter :")
+while choix != 0:
+    print(" \nVeuillez choisir une option\n")
+    print(" 1 - Ajouter un nom:")
+    print(" 2 - Consulter le répertoire:")
+    print(" 0 - Pour quitter :")
+    choix = input("-->")
+    print(f"Votre choix est le {choix}")
